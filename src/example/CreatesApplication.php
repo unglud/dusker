e9ad2078909651f49c287432270fb3b7c710e7cc
Aleksandr <aleksandr.matrosov@check24.de>
2017-11-08 13:20:30 +0100

    Forgot inject Browser

--- src/example/CreatesApplication.php
+++ src/example/CreatesApplication.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Dusker\Browser;
 use Illuminate\Contracts\Console\Kernel;
 
 /**
@@ -16,10 +17,22 @@ trait CreatesApplication
      */
     public function createApplication()
     {
+        /* @noinspection PhpIncludeInspection */
+        /* @noinspection UsingInclusionReturnValueInspection */
         $app = require __DIR__ . '/../vendor/unglud/dusker/src/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    /**
+     * @param $driver
+     *
+     * @return Browser
+     */
+    protected function newBrowser($driver): Browser
+    {
+        return new Browser($driver);
     }
 }
